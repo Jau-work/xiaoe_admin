94791b02c12bede1eb70a3c9f9caf072576f0d72
Jau <job_jausaihou@163.com>
2021-04-12 20:44:41 +0800

    feat: 课程列表分页与条件搜索实现

--- src/views/course/list.vue
+++ src/views/course/list.vue
@@ -1,6 +1,48 @@
 <template>
   <div class="app-container">
-    <!--查询表单 TODO-->
+    <!--查询表单-->
+    <el-form :inline="true" class="demo-form-inline">
+      <!-- 所属分类：级联下拉列表 -->
+      <!-- 一级分类 -->
+      <el-form-item label="课程类别">
+        <el-select
+          v-model="searchObj.subjectParentId"
+          placeholder="请选择"
+          @change="subjectLevelOneChanged">
+          <el-option
+            v-for="subject in subjectList"
+            :key="subject.id"
+            :label="subject.title"
+            :value="subject.id"/>
+        </el-select>
+        <!-- 二级分类 -->
+        <el-select v-model="searchObj.subjectId" placeholder="请选择">
+          <el-option
+            v-for="subject in subjectLevelTwoList"
+            :key="subject.id"
+            :label="subject.title"
+            :value="subject.id"/>
+        </el-select>
+      </el-form-item>
+      <!-- 标题 -->
+      <el-form-item>
+        <el-input v-model="searchObj.title" placeholder="课程标题"/>
+      </el-form-item>
+      <!-- 讲师 -->
+      <el-form-item>
+        <el-select
+          v-model="searchObj.teacherId"
+          placeholder="请选择讲师">
+          <el-option
+            v-for="teacher in teacherList"
+            :key="teacher.id"
+            :label="teacher.name"
+            :value="teacher.id"/>
+        </el-select>
+      </el-form-item>
+      <el-button type="primary" icon="el-icon-search" @click="fetchData()">查询</el-button>
+      <el-button type="default" @click="resetData">重置</el-button>
+    </el-form>
 
     <!-- 表格 -->
     <el-table :data="list" border stripe>
@@ -74,11 +116,23 @@
       </el-table-column>
     </el-table>
 
-    <!-- 分页 TODO -->
+    <!-- 分页组件 -->
+    <el-pagination
+      :current-page="page"
+      :total="total"
+      :page-size="limit"
+      :page-sizes="[5, 10, 20, 30, 40, 50, 100]"
+      style="padding: 30px 0; text-align: center;"
+      layout="total, sizes, prev, pager, next, jumper"
+      @size-change="changePageSize"
+      @current-change="changeCurrentPage"
+    />
   </div>
 </template>
 <script>
 import courseApi from '@/api/course'
+import teacherApi from '@/api/teacher'
+import subjectApi from '@/api/subject'
 
 export default {
   data() {
@@ -87,21 +141,65 @@ export default {
       total: 0, // 总记录数
       page: 1, // 页码
       limit: 10, // 每页条数
-      searchObj: {}, // 查询条件
+      searchObj: {
+        subjectId: ''
+      }, // 查询条件
       teacherList: [], // 讲师列表
       subjectList: [], // 一级分类列表
       subjectLevelTwoList: [] // 二级分类列表
     }
   },
   created() {
-    this.fetchData() // 显示课程列表
+    // 显示课程列表
+    this.fetchData()
+    // 初始化分类列表
+    this.initSubjectList()
+    // 获取讲师列表
+    this.initTeacherList()
   },
   methods: {
+    // 获取课程列表
     fetchData() {
       courseApi.getPageList(this.page, this.limit, this.searchObj).then(res => {
         this.list = res.data.rows
         this.total = res.data.total
       })
+    },
+    // 获取讲师列表
+    initTeacherList() {
+      teacherApi.list().then(response => {
+        this.teacherList = response.data.items
+      })
+    },
+    // 获取课程分类列表
+    initSubjectList() {
+      subjectApi.getNestedTreeList().then(response => {
+        this.subjectList = response.data.items
+      })
+    },
+    subjectLevelOneChanged(value) {
+      for (let i = 0; i < this.subjectList.length; i++) {
+        if (this.subjectList[i].id === value) {
+          this.subjectLevelTwoList = this.subjectList[i].children
+          this.searchObj.subjectId = ''
+        }
+      }
+    },
+    // 重置表单
+    resetData() {
+      this.searchObj = {} // 清空查询条件
+      this.subjectLevelTwoList = [] // 二级分类列表
+      this.fetchData() // 回显数据
+    },
+    // 每页记录数改变，size：回调参数，表示当前选中的“每页条数”
+    changePageSize(size) {
+      this.limit = size
+      this.fetchData()
+    },
+    // 改变页码，page：回调参数，表示当前选中的“页码”
+    changeCurrentPage(page) {
+      this.page = page
+      this.fetchData()
     }
   }
 }
