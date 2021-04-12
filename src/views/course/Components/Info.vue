cb3a6c290c3ffbab43a21312a300bad7a3423d86
Jau <job_jausaihou@163.com>
2021-04-12 15:41:34 +0800

    feat: 课程类别级联下拉列表实现

--- src/views/course/Components/Info.vue
+++ src/views/course/Components/Info.vue
@@ -14,7 +14,31 @@
         </el-select>
       </el-form-item>
 
-      <!-- 所属分类 TODO -->
+      <!-- 所属分类：级联下拉列表 -->
+      <el-form-item label="课程类别">
+        <!-- 一级分类 -->
+        <el-select
+          v-model="courseInfo.subjectParentId"
+          placeholder="请选择"
+          @change="subjectChanged">
+          <el-option
+            v-for="subject in subjectList"
+            :key="subject.id"
+            :label="subject.title"
+            :value="subject.id"/>
+        </el-select>
+
+        <!-- 二级分类 -->
+        <el-select
+          v-model="courseInfo.subjectId"
+          placeholder="请选择">
+          <el-option
+            v-for="subject in subjectLevelTwoList"
+            :key="subject.id"
+            :label="subject.title"
+            :value="subject.id"/>
+        </el-select>
+      </el-form-item>
 
       <el-form-item label="总课时">
         <el-input-number :min="0" v-model="courseInfo.lessonNum" controls-position="right" placeholder="请填写课程的总课时数"/>
@@ -38,6 +62,7 @@
 <script>
 import courseApi from '@/api/course'
 import teacherApi from '@/api/teacher'
+import subjectApi from '@/api/subject'
 
 export default {
   data() {
@@ -53,13 +78,17 @@ export default {
         cover: '',
         description: ''
       },
-      teacherList: [] // 讲师列表
+      teacherList: [], // 讲师列表
+      subjectList: [], // 一级分类
+      subjectLevelTwoList: [] // 二级分类
     }
   },
   created() {
     this.initTeacherList()
+    this.initSubjectList()
   },
   methods: {
+    // 获取讲师列表
     initTeacherList() {
       teacherApi.list().then(res => {
         this.teacherList = res.data.items
@@ -79,6 +108,25 @@ export default {
     },
     updateData() {
 
+    },
+    // 获取课程分类列表
+    initSubjectList() {
+      subjectApi.getNestedTreeList().then(res => {
+        this.subjectList = res.data.items
+      })
+    },
+    // 切换一级类别下拉列表
+    subjectChanged(value) {
+      // 遍历一级类别
+      this.subjectList.forEach(subject => {
+        // 判断当前选中的一级类别是否与当前遍历的一级类别id一致
+        if (subject.id === value) {
+          // 清空当前二级类别
+          this.courseInfo.subjectId = ''
+          // 如果一致, 则将当前遍历的一级类别的子类别绑定在页面的二级类别列表中
+          this.subjectLevelTwoList = subject.children
+        }
+      })
     }
   }
 }
